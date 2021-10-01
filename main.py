c9c3739acb3d0b895b5a6bb5fe27359504bb9cca
stoktas20 <91738777+stoktas20@users.noreply.github.com>
2021-10-01 14:10:41 +0000

    done for lab1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import numpy
+import numpy as np
 x = int(input("Enter number x here:"))
 y= int(input("Enter number y here:"))
 print(x**y)
-print(numpy.log2(x))
+print(np.log2(x))
 id = 76644
 print(id)
